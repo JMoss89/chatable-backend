f2c2915f43fbfb2e30293992cd453051c86f398a
July Moss <july.x.moss@gmail.com>
2022-05-04 18:59:43 +0100

    added cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
 const dialogFlowRouter = require('./routes/dialogFlowRoutes');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/', dialogFlowRouter);
 // require('./routes/dialogFlowRoutes')(app);
