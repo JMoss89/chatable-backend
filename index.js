c210ca00ed40fabe33d468d7122d4df8dbf7ddc9
July Moss <july.x.moss@gmail.com>
2022-05-13 17:58:02 +0100

    updated mongodb connection string

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require ('./src/app');
 
-const mongoose = require('mongoose');
 const config = require('./config/keys');
+const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, { useNewUrlParser: true }); // code connects to MongoDB
 
 const PORT = process.env.PORT || 4000;
